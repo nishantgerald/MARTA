d696ce210bd9d235d0bb706654f58bb84d6f8220
Preethi Gowrishankar <preethi@Preethis-MacBook-Pro.local>
2019-04-19 08:04:29 -0400

    added register button

--- Files/test.py
+++ Files/test.py
@@ -19,30 +19,34 @@ def login():
                              db='Beltline',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-    username = request.form['username']
-    password = request.form['password']
+    if "login" in request.form:
+        username = request.form['username']
+        password = request.form['password']
 
-    try:
-        with open('MARTA.sql', 'r') as sql:
-            procs = sql.read().split(';')
-            with connection.cursor() as cursor:
-                
-             # Read a single record
-                result = cursor.execute("SELECT Username FROM emails WHERE Email = %s;", [username])
-                user_result = cursor.fetchone()
-                #EVERYTHING UNTIL THIS POINT WORKS-----------------
-            if user_result:
-                with connection.cursor() as cursor2:
-                    pass_result = cursor.execute("SELECT UserType FROM user WHERE Username in SELECT Username FROM emails WHERE Email = %s) AND Password = %s;", [username, password])
-                    return json.dumps(cursor.fetchone())
-            else:
-                return "ERROR: Invalid Username"
+        try:
+            with open('MARTA.sql', 'r') as sql:
+                procs = sql.read().split(';')
+                with connection.cursor() as cursor:
+                    
+                 # Read a single record
+                    result = cursor.execute("SELECT Username FROM emails WHERE Email = %s;", [username])
+                    user_result = cursor.fetchone()
+                    return json.dumps(user_result)
+                    #EVERYTHING UNTIL THIS POINT WORKS-----------------
+                #if user_result:
+                #    with connection.cursor() as cursor2:
+                #        pass_result = cursor.execute("SELECT UserType FROM user WHERE Username in SELECT Username FROM emails WHERE Email = %s) AND Password = %s;", [username, password])
+                #        return json.dumps(cursor.fetchone())
+                #else:
+                #    return "ERROR: Invalid Username"
 
-    except:
-        return "ERROR"
-        
-    finally:
-        connection.close()
+        except:
+            return "ERROR"
+            
+        finally:
+            connection.close()
+    elif "register" in request.form:
+        return render_template('success.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
