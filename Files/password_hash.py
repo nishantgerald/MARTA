663136dc848969426e7ad9e860edd32ac8979002
Jacob Feldman <jfeldman@gatech.edu>
2019-04-20 18:28:02 -0400

    password hashing functions

---
+++ Files/password_hash.py
@@ -0,0 +1,20 @@
+import passlib
+from passlib.hash import sha256_crypt
+
+def hash_password(password):
+	"""
+	Function to encrypt a password
+	"""
+	return sha256_crypt.encrypt(password)
+
+def check_password(password, encrypted_password):
+	"""
+	Returns True if entered password matches encrypted password
+	"""
+	return sha256_crypt.verify(password, encrypted_password)
+
+if __name__ == "__main__":
+
+	hashed_password = hash_password("p@ssw0rd")
+
+	print(check_password("p@ssw0rd", hashed_password))
