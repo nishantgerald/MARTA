56f9b24e2a7be74e5bb1b4a8e37487ad1d2b6aa5
Preethi Gowrishankar <preethi@Preethis-MacBook-Pro.local>
2019-04-20 10:02:38 -0400

    edits

--- Files/test.py
+++ Files/test.py
@@ -4,11 +4,18 @@ import json
 
 from flask import Flask
 app = Flask(__name__, template_folder='template')
-# Connect to the database
+
 user_email = ""
 user_type = ""
 employee_type = ""
 username = ""
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='root',
+                             db='Beltline',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/')
 def index():
@@ -16,12 +23,6 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='root',
-                             db='Beltline',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
     if "login" in request.form:
         user_email = request.form['username']
         password = request.form['password']
